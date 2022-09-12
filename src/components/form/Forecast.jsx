92dc083bd0ab16f009657f17b98eb1a81cf434c4
tzyan35 <zhongyan1997@hotmail.com>
2022-09-12 13:54:14 +0800

    commit display trafficImages

---
+++ src/components/form/Forecast.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const Forecast = ({forecastInfo, location}) => {
+    const [forecast, setForecast] = useState()
+
+    function checkForecast() {
+
+    }
+
+    console.log(forecastInfo)
+    
+
+
+  return (
+    <div>Forecast: </div>
+  )
+}
+
+export default Forecast
