8ffc0566c9491e494e32f31081d0adac8e63189f
tzyan35 <zhongyan1997@hotmail.com>
2022-09-12 22:03:45 +0800

    removed unnecessary folder structure

---
+++ src/components/DateTime.jsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import dayjs from "dayjs";
+import TextField from "@mui/material/TextField";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import { useState, useEffect } from "react";
+import Location from "./Location";
+
+export default function BasicDateTimePicker({}) {
+  const [value, setValue] = useState(dayjs());
+  const [localTime, setLocalTime] = useState("");
+
+  function toLocalTime() {
+    try {
+      const offset = new Date(value.$d).getTimezoneOffset() * 60000;
+      const time = new Date(value.$d - offset)
+        .toISOString()
+        .replace(".000Z", "");
+      setLocalTime(
+        time.split(":")[0] +
+          "%3A" +
+          time.split(":")[1] +
+          "%3A" +
+          time.split(":")[2]
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    toLocalTime();
+  }, [value]);
+
+  return (
+    <div>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateTimePicker
+          renderInput={(props) => <TextField {...props} />}
+          label="Date and Time Picker"
+          value={value}
+          inputFormat="YYYY/MM/DD hh:mm:ss a"
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          minDateTime={dayjs("2016-03-01T00:00")}
+          maxDateTime={dayjs()}
+          hideTabs
+        />
+        {value > dayjs() || value.$d < dayjs("2016-03-01T00:00") ? (
+          <h4>Please input a valid date</h4>
+        ) : (
+          <Location time={localTime} />
+        )}
+      </LocalizationProvider>
+    </div>
+  );
+}
